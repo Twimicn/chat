ebe31a3e168c8c18d1738ea79523ee21011ac251
IvanLuLyf <835498692@qq.com>
2020-01-24 01:27:00 +0800

    v25 Video

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CATCH_VER = 'v24';
+const CATCH_VER = 'v25';
 self.addEventListener('install', function (event) {
     let languages = ['zh-cn', 'en-us'];
     let languageCode = (navigator.language || 'zh-cn').toLocaleLowerCase();
