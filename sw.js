65e581465830269a325d2385e851a4a7eedd8733
IvanLuLyf <835498692@qq.com>
2020-02-12 02:34:58 +0800

    update Style

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CATCH_VER = 'v30';
+const CATCH_VER = 'v31';
 self.addEventListener('install', function (event) {
     let languages = ['zh-cn', 'en-us'];
     let languageCode = (navigator.language || 'zh-cn').toLocaleLowerCase();
